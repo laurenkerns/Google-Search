bbccd233b9bac04922380e443650502cb06c514d
laurenkerns <laurenkerns@gmail.com>
2019-04-30 14:18:45 -0400

    file structure from activity 7

---
+++ google-search/client/build/precache-manifest.6841eed6b616759a78eaa3e6914ef487.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0daedd6dd2e60f7be4d9",
+    "url": "/static/js/main.146b29be.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "0daedd6dd2e60f7be4d9",
+    "url": "/static/css/main.b3b15cdd.chunk.css"
+  },
+  {
+    "revision": "c01eeb03d2669ec802f876301f4cf647",
+    "url": "/index.html"
+  }
+];
